1b3e2aea2ff8dba915522f487d891a41f8bbaca9
Benedek Virág <viragbenedek06@gmail.com>
2024-11-10 01:33:33 +0100

    function generate table létrehozása. Elakadtam a táblázat generálásában....

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 const data = [
-    { uralkodo_nev: 'I. István', esemeny1: 'Koronázás', evszam1: '1000', esemeny2: 'Pannonhalmi apátság megalapítása', evszam2: '1001' },
-    { uralkodo_nev: 'IV. Béla', esemeny1: 'Tatárjárás', evszam1: '1241-1242' },
-    { uralkodo_nev: 'Mátyás király', esemeny1: 'Bécs elfoglalása', evszam1: '1485', esemeny2: 'Kenyérmezei csata', evszam2: '1479' },
-    { uralkodo_nev: 'II. Rákóczi Ferenc', esemeny1: 'A szabadságharc kezdete', evszam1: '1703', esemeny2: 'A szabadságharc vége', evszam2: '1711' }
+    {
+        uralkodo_nev: 'I. István', 
+        esemeny1: 'Koronázás', 
+        evszam1: '1000', 
+        esemeny2: 'Pannonhalmi apátság megalapítása', 
+        evszam2: '1001' 
+    },
+    { 
+        uralkodo_nev: 'IV. Béla', 
+        esemeny1: 'Tatárjárás', 
+        evszam1: '1241-1242' 
+    },
+    { 
+        uralkodo_nev: 'Mátyás király', 
+        esemeny1: 'Bécs elfoglalása', 
+        evszam1: '1485', 
+        esemeny2: 'Kenyérmezei csata', 
+        evszam2: '1479' },
+    { 
+        uralkodo_nev: 'II. Rákóczi Ferenc', 
+        esemeny1: 'A szabadságharc kezdete', 
+        evszam1: '1703', 
+        esemeny2: 'A szabadságharc vége', 
+        evszam2: '1711' 
+    }
 ];
+
+function GenerateTable(){
+    for (const tortenelem of data) {
+        const tr = document.createElemnt('tr')
+        tablebody.appendChild(tr)
+        const uralkodo_nev = document.createElement('td')
+
+        tr.appendChild(uralkodo_nev)
+        uralkodo_nev.innerHTML = tortenelem.uralkodo_nev
+    }
+}
