4a10af62363cc5df836371d11e8ffbf957aed208
Benedek Virág <viragbenedek06@gmail.com>
2024-11-10 01:03:56 +0100

    Adatok definiálása

---
+++ script.js
@@ -0,0 +1,6 @@
+const data = [
+    { uralkodo_nev: 'I. István', esemeny1: 'Koronázás', evszam1: '1000', esemeny2: 'Pannonhalmi apátság megalapítása', evszam2: '1001' },
+    { uralkodo_nev: 'IV. Béla', esemeny1: 'Tatárjárás', evszam1: '1241-1242' },
+    { uralkodo_nev: 'Mátyás király', esemeny1: 'Bécs elfoglalása', evszam1: '1485', esemeny2: 'Kenyérmezei csata', evszam2: '1479' },
+    { uralkodo_nev: 'II. Rákóczi Ferenc', esemeny1: 'A szabadságharc kezdete', evszam1: '1703', esemeny2: 'A szabadságharc vége', evszam2: '1711' }
+];
